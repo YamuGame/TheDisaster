c9eedd42e97fded8ae56391941b3613f31c8c643
C0116168 <C0116168@DESKTOP-JLJ4J9G>
2018-12-07 16:36:45 +0900

    修正

--- TheDisaster/src/thedisaster/TheDisaster.java
+++ TheDisaster/src/thedisaster/TheDisaster.java
@@ -99,7 +99,7 @@ public class TheDisaster {
         Story.stage3After(); //ステージ3戦後
 
         Story.security(); //イージマス前
-        EasyMass(); //イージマス
+        EasyMass.easyMass(); //イージマス
 
         Story.lastBattle(); //ラスト前
         if (hasGotWeapon_toyKnife
@@ -221,70 +221,6 @@ public class TheDisaster {
             }
         }
         Disaster.SetName(DisasterName);
-    }
-
-    /**
-     * イージーマス
-     */
-    public static void EasyMass() {
-        System.out.println(""
-                + "※簡単な数学の問題が5問表示されます\n"
-                + "※半角数字で回答を入力してください▼");
-        scan.nextLine();
-
-        System.out.println(""
-                + "※回答を間違えると体力が減少します\n"
-                + "※正解するまで問題が変わることはありません▼");
-        scan.nextLine();
-
-        System.out.println("START▼");
-
-        Random rand = new Random();//ランダムメソッド
-        int[] num = new int[2];//ランダム値保存用
-        int ans = 0;//答え保存用
-        int sisoku = 0;//四則種類選択用
-        String question = null;//問題内容保存用
-        char loop = 0;//繰り返し回数保存用
-        do {//do/while文で5問繰り返す
-            for (int i = 0; i < 2; i++) {//２つの数値を保存
-                int q = rand.nextInt(13);
-                num[i] = q;
-            }
-            sisoku = rand.nextInt(3);//ランダムに＋ー×を選択
-            switch (sisoku) {
-                case 0:
-                    ans = num[0] + num[1];
-                    question = "+";
-                    break;
-                case 1:
-                    ans = num[0] - num[1];
-                    question = "-";
-                    break;
-                case 2:
-                    ans = num[0] * num[1];
-                    question = "×";
-                    break;
-            }
-            question = "[第" + String.valueOf(loop + 1) + "問]" + String.valueOf(num[0]) + question + String.valueOf(num[1]) + "=";
-            System.out.println(question);
-            boolean correct = true;
-            while (correct) {
-                try {
-                    String str = scan.nextLine();
-                    if (str.equals(String.valueOf(ans))) {
-                        System.out.println("正解");
-                        correct = false;
-                    } else {
-                        System.out.println("不正解");
-                        Disaster.SetHP(-100);
-                    }
-                } catch (Exception e) {
-                }
-            }
-            loop++;
-        } while (loop < 5);
-        System.out.println("セキュリティーチェック完了");
-        System.out.println("HP : " + Disaster.getHP());
     }
 
     /**
